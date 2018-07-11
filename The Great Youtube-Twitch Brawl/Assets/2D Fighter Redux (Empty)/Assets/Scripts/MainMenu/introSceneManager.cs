1b5bae732849df7ec654d4d2caa5ebb3fba7f4ac
Potatoes <ibznakhal@gmail.com>
2018-07-10 19:21:45 -0700

    scene randomized is done, needs random images for scenes.

---
+++ The Great Youtube-Twitch Brawl/Assets/2D Fighter Redux (Empty)/Assets/Scripts/MainMenu/introSceneManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class introSceneManager : MonoBehaviour {
+
+    public GameObject startText;
+    float timer;
+    bool loadingLevel;
+    bool init;
+
+    public int activeElement;
+    public GameObject menuObj;
+    public ButtonRef[] menuOptioins;
+
+	// Use this for initialization
+	void Start () {
+        menuObj.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!init)
+            {
+            timer += Time.deltaTime;
+            if(timer>0.6f)
+            {
+                timer = 0;
+                startText.SetActive(!startText.activeInHierarchy);
+            }
+
+            if()
+
+        }
+	}
+}
---
+++ The Great Youtube-Twitch Brawl/Assets/2D Fighter Redux (Empty)/Assets/Scripts/Utilities/MenuImageRandomizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuImageRandomizer : MonoBehaviour {
+    public List<Sprite> images;
+
+	// Use this for initialization
+	void Start () {
+        int rando = Random.RandomRange(0, images.Count);
+        this.GetComponent<Image>().sprite = images[rando];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
